3bda0740e41ceeb08028addf43684a6f0f35d5e7
Mr-Ren-kw <335614025@qq.com>
2019-10-02 23:47:18 +0800

    user:renkw----only orderDetail lost

--- src/main/java/com/j4h/mall/mapper/mall/CategoryMapper.java
+++ src/main/java/com/j4h/mall/mapper/mall/CategoryMapper.java
@@ -1,7 +1,11 @@
 package com.j4h.mall.mapper.mall;
 
+import com.j4h.mall.model.mall.category.AddCategory;
 import com.j4h.mall.model.mall.category.L1Name;
 import com.j4h.mall.model.mall.category.L1Category;
+import com.j4h.mall.vo.mall.category.AddCategoryVo;
+import com.j4h.mall.vo.mall.category.UpdateCategoryVo;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -9,4 +13,14 @@ public interface CategoryMapper {
     List<L1Name> getL1Category();
 
     List<L1Category> queryAllCategory();
+
+    int createCategory(@Param("new") AddCategoryVo addCategoryVo,@Param("result") AddCategory addCategory);
+
+    AddCategory queryCategoryById(@Param("id") int id);
+
+    int updateCategory(@Param("update") UpdateCategoryVo updateCategoryVo);
+
+    int deleteCategoryByPid(@Param("pid") int pid);
+
+    int deleteCategoryById(@Param("id") int id);
 }
