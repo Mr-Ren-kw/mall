870b66fc6ff00631011ea01ee7f534b3a9dd27c6
Mr-Ren-kw <335614025@qq.com>
2019-10-08 12:59:33 +0800

    renkw: fix coupon bugs

--- src/main/java/com/j4h/mall/controller/wx/order/WxOrderController.java
+++ src/main/java/com/j4h/mall/controller/wx/order/WxOrderController.java
@@ -23,7 +23,7 @@ public class WxOrderController {
     public BaseRespVo orderList(UserOrderPage userOrderPage) {
         boolean login = LoginOrNotUtils.isLogin();
         if (!login) {
-            return BaseRespVo.fail(505,"请登录后再访问");
+            return BaseRespVo.fail(501,"请登录后再访问");
         }
         AllGoodsList allGoodsList = wxUserOrderService.queryAllOrderList(userOrderPage);
         BaseRespVo<AllGoodsList> ok = BaseRespVo.ok(allGoodsList);
@@ -33,7 +33,7 @@ public class WxOrderController {
     @RequestMapping("wx/order/detail")
     public BaseRespVo orderDetail(int orderId) {
         if (!LoginOrNotUtils.isLogin()) {
-            return BaseRespVo.fail(504, "请登录后再访问");
+            return BaseRespVo.fail(501, "请登录后再访问");
         }
         ResultOrder order = wxUserOrderService.querySingleOrderByOrderId(orderId);
         BaseRespVo<ResultOrder> ok = BaseRespVo.ok(order);
@@ -44,9 +44,15 @@ public class WxOrderController {
     @RequestMapping("wx/order/prepay")
     public BaseRespVo orderPrepay(@RequestBody OrderId orderId) {
         if (!LoginOrNotUtils.isLogin()) {
-            return BaseRespVo.fail(502, "请登录后再访问");
+            return BaseRespVo.fail(501, "请登录后再访问");
         }
-        boolean flag = wxUserOrderService.orderPrepay(orderId.getOrderId());
+        int orderId1;
+        try {
+            orderId1 = Integer.parseInt(orderId.getOrderId());
+        } catch (NumberFormatException e) {
+            return BaseRespVo.fail(402, "参数异常，付款失败");
+        }
+        boolean flag = wxUserOrderService.orderPrepay(orderId1);
         if (flag) {
             return BaseRespVo.ok("付款成功");
         }
@@ -57,9 +63,15 @@ public class WxOrderController {
     @RequestMapping("wx/order/cancel")
     public BaseRespVo orderCancel(@RequestBody OrderId orderId) {
         if (!LoginOrNotUtils.isLogin()) {
-            return BaseRespVo.fail(503, "请登录后再访问");
+            return BaseRespVo.fail(501, "请登录后再访问");
         }
-        boolean flag = wxUserOrderService.orderCancel(orderId.getOrderId());
+        int orderId1;
+        try {
+            orderId1 = Integer.parseInt(orderId.getOrderId());
+        } catch (NumberFormatException e) {
+            return BaseRespVo.fail(402, "参数异常，付款失败");
+        }
+        boolean flag = wxUserOrderService.orderCancel(orderId1);
         if (flag) {
             return BaseRespVo.ok(null);
         }
@@ -71,7 +83,13 @@ public class WxOrderController {
         if (!LoginOrNotUtils.isLogin()) {
             return BaseRespVo.fail(501, "请登录后再访问");
         }
-        boolean flag = wxUserOrderService.orderConfirm(orderId.getOrderId());
+        int orderId1;
+        try {
+            orderId1 = Integer.parseInt(orderId.getOrderId());
+        } catch (NumberFormatException e) {
+            return BaseRespVo.fail(402, "参数异常，付款失败");
+        }
+        boolean flag = wxUserOrderService.orderConfirm(orderId1);
         if (flag) {
             return BaseRespVo.ok(null);
         }
@@ -84,7 +102,13 @@ public class WxOrderController {
         if (userId == null) {
             return BaseRespVo.fail(501, "请登录后再访问");
         }
-        return wxUserOrderService.orderDeleteByOid(orderId.getOrderId()) ? BaseRespVo.ok(null) : BaseRespVo.fail();
+        int orderId2;
+        try {
+            orderId2 = Integer.parseInt(orderId.getOrderId());
+        } catch (NumberFormatException e) {
+            return BaseRespVo.fail(402, "参数异常，付款失败");
+        }
+        return wxUserOrderService.orderDeleteByOid(orderId2) ? BaseRespVo.ok(null) : BaseRespVo.fail();
     }
 
     @RequestMapping("wx/order/refund")
@@ -93,7 +117,13 @@ public class WxOrderController {
         if (userId == null) {
             return BaseRespVo.fail(501, "请登录后再访问");
         }
-        boolean flag = wxUserOrderService.orderRefund(orderId.getOrderId());
+        int orderId3;
+        try {
+            orderId3 = Integer.parseInt(orderId.getOrderId());
+        } catch (NumberFormatException e) {
+            return BaseRespVo.fail(402, "参数异常，付款失败");
+        }
+        boolean flag = wxUserOrderService.orderRefund(orderId3);
         if (flag){
             return BaseRespVo.ok(null);
         }
